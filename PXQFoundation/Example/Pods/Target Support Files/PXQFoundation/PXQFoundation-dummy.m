ad0ec1f743623f03263a3633615001a16639a2b6
xq.pan <xq.pan@ctrip.com>
2020-12-25 17:53:37 +0800

    Foundation init

--- PXQFoundation/Example/Pods/Target Support Files/PXQFoundation/PXQFoundation-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PXQFoundation : NSObject
-@end
-@implementation PodsDummy_PXQFoundation
-@end
--- PXQFoundation/Example/Pods/Target Support Files/Pods-PXQFoundation_Tests/Pods-PXQFoundation_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PXQFoundation_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_PXQFoundation_Tests
-@end
